0fab03b8ca4007bda62147c567e47df2cbef2ba6
Kaileyv <kailey.van@stonybrook.edu>
2021-04-08 16:58:48 -0400

    Initial Commit

---
+++ Foodie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Foodie
+//
+//  Created by Kailey Van on 4/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
